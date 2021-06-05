24b4c223af9305bbe49dc03fd95b6649553e39e7
Trystan Rivers <trystan@bigbrave.co.za>
2021-06-05 16:42:09 +0200

    [F] Added Carousel Images

--- components/Frame.tsx
+++ components/Frame.tsx
@@ -1,49 +1,62 @@
 import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const Frame = () => {
   return (
-    <div
-      className="body  max-w-6xl ml-auto mr-auto"
-      style={{
-        backgroundImage:
-          "url(https://i.ibb.co/nrmkm7d/five-bells-washed-out-logo.png) , linear-gradient(to right, rgba(58, 61, 62, 1) 0%, rgba(58, 61, 62, 1) 100%);",
-      }}
-    >
+    <div className="body  max-w-6xl ml-auto mr-auto">
       <div className="outer-border">
         <div className="mid-border">
-          <div className="inner-border">
+          <div className="inner-border ">
             <img
-              className="corner-decoration corner-left-top"
+              className="corner-decoration corner-left-top z-50"
               src="../corner-decoration-new.png"
             ></img>
             <img
-              className="corner-decoration corner-right-top"
+              className="corner-decoration corner-right-top z-50"
               src="../corner-decoration-new.png"
             ></img>
             <img
-              className="corner-decoration corner-right-bottom"
+              className="corner-decoration corner-right-bottom z-50"
               src="../corner-decoration-new.png"
             ></img>
             <img
-              className="corner-decoration corner-left-bottom"
+              className="corner-decoration corner-left-bottom z-50"
               src="../corner-decoration-new.png"
             ></img>
             <img
-              className="vertical-decoration top"
+              className="vertical-decoration top z-50"
               src="https://i.ibb.co/JRTK9z4/horizontally-centered-vertical-decoration.png"
             ></img>
             <img
-              className="vertical-decoration bottom"
+              className="vertical-decoration bottom z-50"
               src="https://i.ibb.co/JRTK9z4/horizontally-centered-vertical-decoration.png"
             ></img>
 
             <div className="container">
               <div className="row">
-                <div className="flex" style={{ height: "60vh" }}>
-                  <img
-                    src="../grunge-texture-12-1920x1080.jpg"
-                    className="flex-1 object-cover"
-                  />
+                <div className="flex" style={{}}>
+                  <Carousel
+                    showThumbs={false}
+                    showStatus={false}
+                    swipeable={true}
+                    dynamicHeight={false}
+                    emulateTouch={false}
+                    autoPlay={true}
+                    interval={3000}
+                    infiniteLoop
+                    className=" overflow-hidden"
+                  >
+                    <div>
+                      <img src="../images-squoosh/IMG_1119.jpg" />
+                    </div>
+                    <div>
+                      <img src="../images-squoosh/IMG_1117.jpg" />
+                    </div>
+                    <div>
+                      <img src="../images-squoosh/IMG_1118.jpg" />
+                    </div>
+                  </Carousel>
                 </div>
               </div>
             </div>
